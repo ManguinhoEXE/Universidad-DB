45cd18a9f8941330917af6f10b6cba9f12ed6850
manguinho <jpaul1706@hotmail.com>
2024-05-22 16:30:21 -0500

    70%

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,12 +1,11 @@
 import { Fragment } from "react"
-import NavB from "./NavB"
 
 const Home = () => {
     return (
         <Fragment>
-            <header>
-                <NavB />
-            </header>
+            <main>
+
+            </main>
         </Fragment>
     )
 }
--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,26 +1,71 @@
 import { Fragment } from "react"
-import NavB from "./NavB"
+import React from "react"
+import { auth} from '../firebase'
+import { useNavigate } from 'react-router-dom'
+
 
 const SignIn = () => {
+
+    const [email, setEmail] = React.useState('')
+    const [pass, setPass] = React.useState('')
+    const [error, setError] = React.useState(null)
+    const navigate = useNavigate()
+
+    const guardarDatos = (e) => {
+        e.preventDefault()
+        if (!email) return setError("Ingrese su Email")
+        if (!pass) return setError("Ingrese su Password")
+        if (pass.length < 6) return setError("Password mínimo de 6 caracteres")
+
+        setError(null)
+        if (login()) {
+            login()
+        }
+    }
+
+    const login = React.useCallback(async () => {
+        try {
+            const res = await auth.signInWithEmailAndPassword(email, pass)
+            console.log(res.user);
+            setEmail('')
+            setPass('')
+            setError(null)
+            navigate('/Admin')
+        } catch (error) {
+            if (error.code === 'auth/invalid-email') {
+                setError('Email no Válido')
+            }
+            if (error.code === 'auth/user-not-found') {
+                setError('Email no Registrado')
+            }
+            if (error.code === 'auth/wrong-password') {
+                setError('Password no coincide')
+            }
+
+
+        }
+    }, [email, pass, navigate])
+
+
     return (
         <Fragment>
-            <header>
-                <NavB />
-            </header>
             <main className="container-fluid p-5 justify-content-center col-6">
+                {
+                    error && (<div className='alert alert-danger mt-2 col-4'>{error}</div>)
+                }
                 <div className=" container justify-content-center bg bg-light p-5 rounded-5 shadow-lg">
-                    <form>
+                    <form onSubmit={guardarDatos}>
                         <h2 className="text-center">iniciar sesión</h2>
                         <div className="mb-2 col-5 mx-auto">
                             <label className="">Email</label>
-                            <input type="text" className="form-control" />
+                            <input type="text" className="form-control" onChange={e => setEmail(e.target.value)}/>
                         </div>
                         <div className="mb-2 col-5 mx-auto">
                             <label className="">Contraseña</label>
-                            <input type="text" className="form-control" />
+                            <input type="password" className="form-control" onChange={e => setPass(e.target.value)} />
                         </div>
                         <div className="mb-2 col-2 mx-auto">
-                            <button className="btn btn-danger mx-4" type="submit">Registrar</button>
+                            <button className="btn btn-danger mx-4" type="button" onClick={login}>Registrar</button>
                         </div>
                     </form>
                 </div>
